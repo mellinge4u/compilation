8bf1c08a7fdd99dd1a05772ce9a5792f8b7700d0
Raphaël <raphael.Merkling@gmail.com>
2015-05-13 20:05:01 +0200

    Juste une suppression de truc innutile

--- src/compiler/Systeme.java
+++ src/compiler/Systeme.java
@@ -5,11 +5,4 @@ public abstract class Systeme extends AbstractTree {
 	public Systeme() {
 		// TODO Auto-generated constructor stub
 	}
-
-	@Override
-	public String getCompiledCode(Compteur i) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
